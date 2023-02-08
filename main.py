60d076edc3f0d84f5767eacfaaad33c623c19cf7
Ivanova Polina <ivanova.p@mail.ru>
2023-02-08 19:50:05 +0300

    new task

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index/<title>')
+def index(title):
     param = {}
-    param['username'] = "Ученик Яндекс.Лицея"
-    param['title'] = 'Домашняя страница'
+    param['username'] = "Mars One"
+    param['title'] = title
     return render_template('index.html', **param)
 
 @app.route('/odd_even')
@@ -35,6 +35,8 @@ def login():
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('login.html', title='Авторизация', form=form)
+# lkdvd;lvv
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
